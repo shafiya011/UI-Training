4de7e0a04a6f858f6f5be5a65044605012e71e2f
shafiya011 <shafiyafareen11@gmail.com>
2023-07-06 08:01:05 +0530

    new commit

--- javascript/To-Do list.js
+++ javascript/To-Do list.js
@@ -1,9 +1,10 @@
 var enterInput=document.getElementById('inputfield');
 var enterButton=document.getElementById('btn');
 let list=document.getElementById('list');
-let deleteIcon=document.getElementsByClassName('deletebutton');
+var items=document.getElementById('l_id')
+var deleteIcon = document.getElementsByClassName("deletebutton");
 
-var btnid=0;
+var listid=0;
 
 function addTask(){
 if(enterInput.value==''){
@@ -13,25 +14,37 @@ else{
     let li = document.createElement('li')
     li.innerHTML=enterInput.value;
     list.appendChild(li);
-    li.id="item"+btnid;
-    btnid++;
-    let button=document.createElement('button');
 
-    let img=document.createElement('IMG');
-    img.src="./pngwing.com (1).png"
-    img.width=10
-    img.height=10
-    button.appendChild(img);
+    li.id="item"+listid;
+    listid++;
+
+    let l_id=li.id;
+    
+    
+    
+    let button=document.createElement('button');
     button.className="deletebutton"
     li.appendChild(button);
+    let img=document.createElement('IMG');
+    img.src="./pngwing.com (1).png";
+    button.appendChild(img);  
+    
+    button.onclick=function(){
+        console.log(l_id);
+        list.removeChild(li);
+    
+
+    }
+   
 }
 enterInput.value='';
-save();
-// showcontent();
+// save();
 }
 
-
-    
+// deleteIcon.onclick()
+// deleteIcon.addEventListener('click', function(e){
+//     e.target.parentElement.remove(li);
+// } )
 
 // function save(){
 //     localStorage.setItem("data",list.innerHTML);
