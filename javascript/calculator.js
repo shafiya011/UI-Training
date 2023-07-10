6250ee285fb3ee5bdab34506e12942d39f4a9b87
shafiya011 <shafiyafareen11@gmail.com>
2023-07-10 17:26:31 +0530

    new commit

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,1 +1,46 @@
-console.log(document.getElementsByTagName('span')[1]);
+// // console.log(document.getElementsByTagName('span')[1]);
+// function fun(src,callback){
+//     var Script = document.createElement('Script');
+//     Script.src = src;
+//      Script.onload=()=>{
+//         console.log("script loaded = ", src);
+//         callback();
+//      }
+//      document.body.appendChild(Script)
+// }
+// fun('https://www.javatpoint.com/react-axios#:~:text=Overview,normally%20shown%20on%20the%20website', fun2);
+
+// function fun2(){
+//     console.log("hii im fun2");
+// }
+
+
+// Promises
+
+let p1=  new Promise(function (resolve,reject)
+{
+    console.log("promise is pending");
+    setTimeout(()=>{
+        alert("im an alert inside promise")
+        console.log("hello");
+        resolve(true)
+        // reject(new Error("im a new error"))
+    },3000)
+})
+let p2=  new Promise(function (resolve,reject)
+{
+    // console.log("promise is pending");
+    setTimeout(()=>{
+        // alert("im an alert2 inside promise")
+        console.log("hello2");
+        // resolve(true)
+        reject(new Error("im a new error2"))
+    },3000)
+})
+console.log(p1);
+console.log(p2);
+
+p1.then((value)=>{
+    console.log("im inside then");
+    console.log(value)
+})
